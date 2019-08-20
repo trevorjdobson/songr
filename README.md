521039cba16d66d7cfa4bef057b03a3e0d5ab878
trevorjdobson <trevorjdobson@gmail.com>
2019-08-20 09:59:05 -0700

    lab complete test working

---
+++ README.md
@@ -0,0 +1,10 @@
+# Songr
+
+## Running and Testing
+* Clone repo to machine
+* CD into directory songr
+* For testing run ./gradlew test
+* For running run ./gradlew bootRun
+    * open localhost:8080/hello
+    * localhost:8080/capitalize/Anthing you want
+    * localhost:8080/reverse?sentence=Any sentance you want to be reversed
---
+++ src/main/java/com/trevorjdobson/songr/HomeController.java
@@ -0,0 +1,44 @@
+package com.trevorjdobson.songr;
+
+
+import org.apache.tomcat.util.codec.binary.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/hello")
+    public String getRoot(){
+
+        return "helloworld";
+    }
+    @GetMapping("/capitalize/{word}")
+    public String capitalize(@PathVariable String word, Model m){
+        String capWord = word.toUpperCase();
+        m.addAttribute("word",capWord);
+
+        return "capitalize";
+    }
+    @GetMapping("/reverse")
+    public String reverse(@RequestParam(required=true) String sentence, Model m){
+        String reversedSentence = reverseString(sentence);
+
+        m.addAttribute("reversedSentance",reversedSentence);
+
+        return "reversed";
+    }
+    public String reverseString(String stringToBeReversed){
+        String words[]=stringToBeReversed.split("\\s");
+        String reversedSentence = "";
+        for(int i = words.length-1;i>=0;i--){
+            reversedSentence+= words[i] + " ";
+        }
+        reversedSentence = reversedSentence.substring(0, reversedSentence.length() - 1);
+        return reversedSentence;
+    }
+
+}
