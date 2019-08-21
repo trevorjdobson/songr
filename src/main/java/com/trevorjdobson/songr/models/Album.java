0b40bb1ea0bab1d2c7f13bd2c613105daa18ccf0
trevorjdobson <trevorjdobson@gmail.com>
2019-08-21 10:22:00 -0700

    lab complete updated readme with further instructions

---
+++ src/main/java/com/trevorjdobson/songr/models/Album.java
@@ -0,0 +1,75 @@
+package com.trevorjdobson.songr.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String title;
+    String artist;
+    int songCount;
+    int length;
+    String imageUrl;
+
+    public Album(){}
+
+    public Album(String title, String artist, int songCount, int length, String imageUrl){
+
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String url) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ src/main/java/com/trevorjdobson/songr/models/AlbumRepository.java
@@ -0,0 +1,6 @@
+package com.trevorjdobson.songr.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+}
